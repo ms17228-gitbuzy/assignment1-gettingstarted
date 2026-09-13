2d2d35d5cadbf36449bae91c5ff583eab5ff8ba7
ms17228-gitbuzy <ms17228@nyu.edu>
2026-09-12 22:09:57 -0400

    Refactor welcome_assignment_answers with complete answers
    Updated the welcome_assignment_answers function to include answers for all nine questions. Enhanced error handling for unknown questions.

--- gettingStarted.py
+++ gettingStarted.py
@@ -33,3 +33,49 @@ if __name__ == "__main__":
 #"Is MD5 a secured hashing algorithm? - Yes/No":
 #"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+
+def welcome_assignment_answers(question):
+ 
+    if question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+        answer = "pcap"
+ 
+    elif question == "Are encoding and encryption the same? - Yes/No":
+        answer = "No"
+ 
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "No"
+ 
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "Yes"
+ 
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+ 
+    elif question == "What is the SHA256 hashing value of your NYU email and use the answer in your code - ":
+        answer = "fdeebff3a99b6584282becf0c301869f43e0d3a8dad5630dd5e8c1af1c1bae43"
+ 
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+ 
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+        answer = 5
+ 
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
+ 
+    else:
+        ### Catches typos / unknown questions so the failure is obvious
+        ### instead of silently returning a wrong answer.
+        answer = "This is not my beautiful wife! This is not my beautiful car! How did I get here?"
+ 
+    return(answer)
+ 
+ 
+if __name__ == "__main__":
+    #use this space to debug and verify that the program works
+ 
+    # Helper: run this once, then paste the printed value into the answer above.
+    my_email = "ms17228@nyu.edu"
+    print("SHA256 of", my_email, "=", hashlib.sha256(my_email.encode()).hexdigest())
+
+
